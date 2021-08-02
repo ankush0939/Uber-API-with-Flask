c96ee743d16ec87a24c32f9619eff8b45b913989
Ankush Mishra <ankushmishra0939@gmail.com>
2021-08-03 00:28:32 +0530

    update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='UberSample',
+    version='0.0.1',
+    author='Ankush Mishra',
+    author_email='ankushmishra0939@gmail.com',
+    packages=find_packages(),
+    description='Flask application',
+)
